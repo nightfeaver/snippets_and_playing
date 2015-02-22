1ab7a948c05e66b0fa5eb9352dea788c62ab692a
nightfeaver <nightfeaver99@gmail.com>
2015-02-22 16:47:08 +0000

    Added a timestamp

--- javascript.js
+++ javascript.js
@@ -5,13 +5,8 @@
 // set page color
 document.body.style.backgroundColor = "cornsilk";
 
-// select element and assign to pressit
-var pressit = document.getElementById('button1');
-
-// create a function for when button pressed
-var checking = function thereaction() {alert('boom')};
-
-// put event handler on button1 element and link to function for when button pressed
-pressit.onclick = checking;
+var timestamp = new Date();
+console.log(timestamp);
+document.getElementById("dateTime").innerHTML += timestamp;
 
 
